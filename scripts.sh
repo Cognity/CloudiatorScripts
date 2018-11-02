c7c600cba53d503089d21fcfcc77077a4f815430
Viktoras Mpenetatos <vmpenetatos@cognity.gr>
2018-11-02 11:27:10 +0200

    Removed call to installDP script

--- scripts.sh
+++ scripts.sh
@@ -10,7 +10,6 @@ sudo -E add-apt-repository -y "deb [arch=amd64] https://download.docker.com/linu
 sudo -E apt-get -y update
 sudo -E apt-get -y --allow-unauthenticated install docker-ce
 
-sudo ./installDP.sh
 }
 
 start() {
